941e2c1c7f5f7c653ba70c43d73b506622832d28
Aleksey Koryakin <aleksey.koryakin@skillbox.ru>
2022-07-10 22:21:32 +0300

    Начинаем программировать

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,19 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type Place = {
+  id: number;
+  price: number;
+  rating: number;
+  title: string;
+  type: string;
+}
+
+type AppProps = {
+  places: Place[]
+}
+
+function App({places}: AppProps): JSX.Element {
+  return <MainScreen places={places} />;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const places = [
+  {
+    id: 10,
+    price: 180,
+    rating: 70,
+    title: 'Beautiful & luxurious apartment at great location',
+    type: 'Apartment',
+  },
+  {
+    id: 20,
+    price: 80,
+    rating: 99,
+    title: 'Beautiful & luxurious apartment at great location',
+    type: 'Apartment',
+  },
+  {
+    id: 30,
+    price: 40,
+    rating: 10,
+    title: 'Beautiful & luxurious apartment at great location',
+    type: 'Apartment',
+  },
+  {
+    id: 40,
+    price: 580,
+    rating: 40,
+    title: 'Beautiful & luxurious apartment at great location',
+    type: 'Apartment',
+  },
+  {
+    id: 50,
+    price: 860,
+    rating: 100,
+    title: 'Beautiful & luxurious apartment at great location',
+    type: 'Apartment',
+  },
+];
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App places={places} />
+  </React.StrictMode>
 );
